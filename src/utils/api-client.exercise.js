008225cfb2e2b19f4cd2591fe60ba3500945a5fc
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-09-21 00:26:00 +0430

    handle failed requests

--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -1,10 +1,4 @@
 function client(endpoint, customConfig = {}) {
-  // 🐨 create the config you'll pass to window.fetch
-  //    make the method default to "GET"
-  // 💰 if you're confused by this, that's fine. Scroll down to the bottom
-  // and I've got some code there you can copy/paste.
-  // then handle the json response
-
   const config = {
     method: 'GET',
     ...customConfig,
@@ -12,108 +6,14 @@ function client(endpoint, customConfig = {}) {
 
   return window
     .fetch(`${process.env.REACT_APP_API_URL}/${endpoint}`, config)
-    .then(res => {
-      return res.json()
+    .then(async response => {
+      const data = await response.json()
+      if (response.ok) {
+        return data
+      } else {
+        return Promise.reject(data)
+      }
     })
-  // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-  // 💰 here's how to get the full URL: `${process.env.REACT_APP_API_URL}/${endpoint}`
 }
 
 export {client}
-
-/*
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-💰 spoiler alert below...
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const config = {
-    method: 'GET',
-    ...customConfig,
-  }
-*/
