da2fcf7ad4fdc60e46cec3867b39c7c9153ad7f5
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-09-20 20:46:25 +0430

    making HTTP request

--- src/discover.exercise.js
+++ src/discover.exercise.js
@@ -7,13 +7,19 @@ import {FaSearch} from 'react-icons/fa'
 import {Input, BookListUL, Spinner} from './components/lib'
 import {BookRow} from './components/book-row'
 // 🐨 import the client from './utils/api-client'
+import {client} from './utils/api-client'
+import React from 'react'
 
 function DiscoverBooksScreen() {
   // 🐨 add state for status ('idle', 'loading', or 'success'), data, and query
-  const data = null // 💣 remove this, it's just here so the example doesn't explode
+  // const data = null // 💣 remove this, it's just here so the example doesn't explode
   // 🐨 you'll also notice that we don't want to run the search until the
   // user has submitted the form, so you'll need a boolean for that as well
   // 💰 I called it "queried"
+  const [status, setStatus] = React.useState('idle')
+  const [data, setData] = React.useState(null)
+  const [query, setQuery] = React.useState('')
+  const [queried, setQueried] = React.useState(false)
 
   // 🐨 Add a useEffect callback here for making the request with the
   // client and updating the status and data.
@@ -21,16 +27,31 @@ function DiscoverBooksScreen() {
   // 🐨 remember, effect callbacks are called on the initial render too
   // so you'll want to check if the user has submitted the form yet and if
   // they haven't then return early (💰 this is what the queried state is for).
+  React.useEffect(() => {
+    if (!queried) {
+      return
+    }
+
+    setStatus('loading')
+    client(`books?query=${encodeURIComponent(query)}`).then(data => {
+      setData(data)
+      setStatus('success')
+      console.log(data)
+    })
+  }, [query, queried])
 
   // 🐨 replace these with derived state values based on the status.
-  const isLoading = false
-  const isSuccess = false
+  const isLoading = status === 'loading'
+  const isSuccess = status === 'success'
 
   function handleSearchSubmit(event) {
     // 🐨 call preventDefault on the event so you don't get a full page reload
     // 🐨 set the queried state to true
     // 🐨 set the query value which you can get from event.target.elements
     // 💰 console.log(event.target.elements) if you're not sure.
+    event.preventDefault()
+    setQueried(true)
+    setQuery(event.target.elements.search.value)
   }
 
   return (
--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -3,7 +3,18 @@ function client(endpoint, customConfig = {}) {
   //    make the method default to "GET"
   // 💰 if you're confused by this, that's fine. Scroll down to the bottom
   // and I've got some code there you can copy/paste.
-  // 🐨 call window.fetch(fullURL, config) then handle the json response
+  // then handle the json response
+
+  const config = {
+    method: 'GET',
+    ...customConfig,
+  }
+
+  return window
+    .fetch(`${process.env.REACT_APP_API_URL}/${endpoint}`, config)
+    .then(res => {
+      return res.json()
+    })
   // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
   // 💰 here's how to get the full URL: `${process.env.REACT_APP_API_URL}/${endpoint}`
 }
