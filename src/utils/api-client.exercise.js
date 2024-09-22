4a1cd58011db1ce355feeca6e3c840976fb8976a
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-09-22 12:01:31 +0430

    automatically logout on 401

--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -1,11 +1,15 @@
+import * as auth from 'auth-provider'
 const apiURL = process.env.REACT_APP_API_URL
 
 function client(
   endpoint,
-  {token, headers: customHeaders, ...customConfig} = {},
+  {data, token, headers: customHeaders, ...customConfig} = {},
 ) {
   const config = {
+    method: data ? 'POST' : 'GET',
+    // body: data ? JSON.stringify(data) : undefined,
     headers: {
+      // 'Content-Type': data ? 'application/json' : undefined,
       Authorization: token ? `Bearer ${token}` : undefined,
       ...customHeaders,
     },
@@ -13,6 +17,14 @@ function client(
   }
 
   return window.fetch(`${apiURL}/${endpoint}`, config).then(async response => {
+    if (response.status === 401) {
+      console.log(response)
+
+      await auth.logout()
+      // refresh the page for them
+      window.location.assign(window.location)
+      return Promise.reject({message: 'Please re-authenticate.'})
+    }
     const data = await response.json()
     if (response.ok) {
       return data
