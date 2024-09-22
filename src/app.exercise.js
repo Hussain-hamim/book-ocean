7684cfdccd174453dc460d1a8a840f85fdbbd2c3
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-09-22 11:09:19 +0430

    use useAsync

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -8,6 +8,9 @@ import * as auth from 'auth-provider'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
 import {client} from 'utils/api-client.exercise'
+import {FullPageSpinner} from 'components/lib'
+import {useAsync} from 'utils/hooks'
+import * as colors from './styles/colors'
 
 // fetch('https://example.com/pets', {
 //   headers: {
@@ -29,34 +32,68 @@ async function getUser() {
 
 function App() {
   // 🐨 useState for the user
-  const [user, setUser] = React.useState(null)
+
+  const {
+    data: user,
+    error,
+    isIdle,
+    isLoading,
+    isSuccess,
+    isError,
+    run,
+    setData,
+  } = useAsync()
+
+  // const doSomething = () => somethingAsync().then(data => setData(data))
 
   React.useEffect(() => {
-    getUser().then(u => setUser(u))
-  }, [])
+    run(getUser())
+  }, [run])
 
   // 🐨 create a login function that calls auth.login then sets the user
   // 💰 const login = form => auth.login(form).then(u => setUser(u))
-  const login = form => auth.login(form).then(u => setUser(u))
+  const login = form => auth.login(form).then(u => setData(u))
 
   // console.log(auth.register('hussain', 123))
 
   // 🐨 create a registration function that does the same as login except for register
-  const register = form => auth.register(form).then(u => setUser(u))
+  const register = form => auth.register(form).then(u => setData(u))
 
   // 🐨 create a logout function that calls auth.logout() and sets the user to null
   const logout = () => {
     auth.logout()
-    setUser(null)
+    setData(null)
   }
 
-  // 🐨 if there's a user, then render the AuthenticatedApp with the user and logout
-  // 🐨 if there's not a user, then render the UnauthenticatedApp with login and register
-  return user ? (
-    <AuthenticatedApp user={user} logout={logout} />
-  ) : (
-    <UnauthenticatedApp login={login} register={register} />
-  )
+  if (isLoading || isIdle) {
+    return <FullPageSpinner />
+  }
+
+  if (isError) {
+    return (
+      <div
+        css={{
+          color: colors.danger,
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <p>Uh oh... There's a problem. Try refreshing the app.</p>
+        <pre>{error.message}</pre>
+      </div>
+    )
+  }
+
+  if (isSuccess) {
+    return user ? (
+      <AuthenticatedApp user={user} logout={logout} />
+    ) : (
+      <UnauthenticatedApp login={login} register={register} />
+    )
+  }
 }
 
 export {App}
