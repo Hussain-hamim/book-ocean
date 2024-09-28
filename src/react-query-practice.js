213568f5b2847278b718ca19a382690cec2cc44d
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-09-28 19:30:42 +0430

    onSuccess moment

--- src/react-query-practice.js
+++ src/react-query-practice.js
@@ -47,3 +47,35 @@ function ChangeName({id}) {
     </form>
   )
 }
+
+////
+
+function useUpdateUser() {
+  return useMutation({
+    mutationFn: updateUser,
+  })
+}
+
+function ChangeName({id}) {
+  const {mutate, status} = useUpdateUser()
+
+  return (
+    <form
+      onSubmit={event => {
+        event.preventDefault()
+        const newName = new FormData(event.currentTarget).get('name')
+        mutate(
+          {id, newName},
+          {
+            onSuccess: () => event.currentTarget.reset(),
+          },
+        )
+      }}
+    >
+      <input name="name" />
+      <button type="submit" disabled={status === 'pending'}>
+        {status === 'pending' ? '...' : 'Update'}
+      </button>
+    </form>
+  )
+}
