ca16e3fe370de53d7e405c6774f1a2ff43298653
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-09-28 19:23:38 +0430

    useMutation

--- src/react-query-practice.js
+++ src/react-query-practice.js
@@ -1,1 +1,22 @@
-const {mutate} = useMutation({mutationFn})
+function useUpdateUser() {
+  return useMutation({
+    mutationFn: updateUser,
+  })
+}
+
+function ChangeName({id}) {
+  const {mutate} = useUpdateUser()
+
+  return (
+    <form
+      onSubmit={event => {
+        event.preventDefault()
+        const newName = new FormData(event.currentTarget).get('name')
+        mutate({id, newName})
+      }}
+    >
+      <input name="name" />
+      <button type="submit">Update</button>
+    </form>
+  )
+}
