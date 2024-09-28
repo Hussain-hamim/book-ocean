23a1fbca87c3350e6ed054406862cd1e9025532d
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-09-28 20:57:01 +0430

    invalidateQueries

--- src/react-query-practice.js
+++ src/react-query-practice.js
@@ -101,3 +101,93 @@ function ChangeName({id}) {
     </form>
   )
 }
+
+/////////////////////
+
+import * as React from 'react'
+import {useQuery, useMutation, useQueryClient} from '@tanstack/react-query'
+import {fetchTodos, addTodo} from './api'
+
+function useAddTodo() {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: addTodo,
+    onSuccess: () => {
+      return queryClient.invalidateQueries({
+        queryKey: ['todos', 'list'],
+      })
+    },
+  })
+}
+
+function useTodos(sort) {
+  const queryClient = useQueryClient()
+
+  return useQuery({
+    queryKey: ['todos', 'list', {sort}],
+    queryFn: () => fetchTodos(sort),
+    placeholderData: () => queryClient.getQueryData(['todos', 'list', {sort}]),
+    staleTime: 10 * 1000,
+  })
+}
+
+export default function TodoList() {
+  const [sort, setSort] = React.useState('id')
+  const {status, data, isPlaceholderData} = useTodos(sort)
+  const addTodo = useAddTodo()
+
+  const handleAddTodo = event => {
+    event.preventDefault()
+    const title = new FormData(event.currentTarget).get('add')
+    addTodo.mutate(title, {
+      onSuccess: () => event.target.reset(),
+    })
+  }
+
+  if (status === 'pending') {
+    return <div>...</div>
+  }
+
+  if (status === 'error') {
+    return <div>Error fetching todos</div>
+  }
+
+  return (
+    <div style={{opacity: isPlaceholderData ? 0.8 : 1}}>
+      <label>
+        Sort by:
+        <select
+          value={sort}
+          onChange={event => {
+            setSort(event.target.value)
+          }}
+        >
+          <option value="id">id</option>
+          <option value="title">title</option>
+          <option value="done">completed</option>
+        </select>
+      </label>
+      <ul>
+        {data.map(todo => (
+          <li key={todo.id}>
+            {todo.done ? '✅ ' : '🗒 '}
+            {todo.title}
+          </li>
+        ))}
+      </ul>
+      <form
+        onSubmit={handleAddTodo}
+        style={{opacity: addTodo.isPending ? 0.8 : 1}}
+      >
+        <label>
+          Add:
+          <input type="text" name="add" placeholder="new todo" />
+        </label>
+        <button type="submit" disabled={addTodo.isPending}>
+          Submit
+        </button>
+      </form>
+    </div>
+  )
+}
