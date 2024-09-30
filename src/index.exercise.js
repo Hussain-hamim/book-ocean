e87debdd611d569b218a78a12840cfb9e367ba70
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-09-30 11:07:35 +0430

    Wrap the `<App />` in a `<ReactQueryConfigProvider />`

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -3,12 +3,29 @@ import './bootstrap'
 import * as React from 'react'
 import {createRoot} from 'react-dom/client'
 import {App} from './app'
+import {ReactQueryConfigProvider} from 'react-query'
+
+const queryConfig = {
+  queries: {
+    useErrorBoundary: true,
+    refetchOnWindowFocus: false,
+    retry(failureCount, error) {
+      if (error.status === 404) return false
+      else if (failureCount < 2) return true
+      else return false
+    },
+  },
+}
 
 // ignore the rootRef in this file. I'm just doing it here to make
 // the tests I write to check your work easier.
 export const rootRef = {}
 loadDevTools(() => {
   const root = createRoot(document.getElementById('root'))
-  root.render(<App />)
+  root.render(
+    <ReactQueryConfigProvider config={queryConfig}>
+      <App />,
+    </ReactQueryConfigProvider>,
+  )
   rootRef.current = root
 })
