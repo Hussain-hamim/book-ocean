0d535ed80909218a7ff37a5d715767da2816a03d
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-10-03 22:08:59 +0430

    Production Monitoring

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -2,6 +2,7 @@ import {loadDevTools} from './dev-tools/load'
 import './bootstrap'
 import * as React from 'react'
 import {createRoot} from 'react-dom/client'
+import {Profiler} from 'components/profiler'
 import {App} from './app'
 import {AppProviders} from './context'
 
@@ -11,9 +12,11 @@ export const rootRef = {}
 loadDevTools(() => {
   const root = createRoot(document.getElementById('root'))
   root.render(
-    <AppProviders>
-      <App />
-    </AppProviders>,
+    <Profiler id="App Root" phases={['mount']}>
+      <AppProviders>
+        <App />
+      </AppProviders>
+    </Profiler>,
   )
   rootRef.current = root
 })
