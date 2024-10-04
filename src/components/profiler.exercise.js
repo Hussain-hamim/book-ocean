d33d089fe6e8e4a6c850e58bf5a65e66d1791708
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-10-04 09:47:29 +0430

    init commit by @Hussain-hamim

---
+++ src/components/profiler.exercise.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import {client} from 'utils/api-client'
+
+let queue = []
+
+function sendProfileQueue() {
+  if (!queue.length) {
+    return Promise.resolve({success: true})
+  }
+  const queueToSend = [...queue]
+  queue = []
+  return client('profile', {data: queueToSend})
+}
+
+function Profiler({metadata, phases, ...props}) {
+  function reportProfile(
+    id,
+    phase,
+    actualDuration,
+    baseDuration,
+    startTime,
+    commitTime,
+    interactions,
+  ) {
+    if (!phases || phases.includes(phase)) {
+      queue.push({
+        metadata,
+        id,
+        phase,
+        actualDuration,
+        baseDuration,
+        startTime,
+        commitTime,
+        interactions,
+      })
+    }
+  }
+
+  return <React.Profiler onRender={reportProfile} {...props} />
+}
+
+export {Profiler}
